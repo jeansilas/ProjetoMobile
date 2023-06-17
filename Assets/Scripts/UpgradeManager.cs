2c166a5443efb8b65439b43cdc5eb0a2222927a0
jeansilas <jeansilas2012@gmail.com>
2023-06-17 14:09:39 -0300

    [Sangue de Jesus tem poder]

---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private CountController countController;
+    void Start()
+    {
+        countController = GameObject.Find("Menu").GetComponent<CountController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpgradeClick(int type)
+    {
+        countController.upgradeTemporary(type);
+    }
+}
---
+++ Assets/Scripts/UpgradeObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UpgradeObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite Icon;
+    public string Title;
+    public string ContentText;
+    public int MHValue;
+    public float TimeValue;
+    public int CoinValue;
+    public int type;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
